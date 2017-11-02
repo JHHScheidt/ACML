cdab269acb8082843c5cd26ecffaec4627871ac3
Marciano Geijselaers <MarcianoG@users.noreply.github.com>
2017-11-02 11:34:39 +0100

    Added Edge class

---
+++ src/lab1/Edge.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lab1;
+
+/**
+ *
+ * @author Marciano
+ */
+public class Edge {
+    
+    private Vertex input, output;
+    private double w;
+    
+    public Edge(Vertex input, Vertex output, double w){
+        this.input = input;
+        this.output = output;
+        this.w = w;
+    }
+    
+    public void setWeight(double w){
+        this.w = w;
+    }
+    
+    public Vertex getVertexInput(){
+        return this.input;
+    }
+    public Vertex getVertexOutput(){
+        return this.output;
+    }
+}
