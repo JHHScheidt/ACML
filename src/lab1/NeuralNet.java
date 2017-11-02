1bdf92bfc1249c77c9045bca45e6a6ef1c0d4897
Marciano Geijselaers <MarcianoG@users.noreply.github.com>
2017-11-02 12:08:28 +0100

    Initialise done

--- src/lab1/NeuralNet.java
+++ src/lab1/NeuralNet.java
@@ -6,6 +6,7 @@
 package lab1;
 
 import java.util.ArrayList;
+import java.util.Random;
 
 /**
  *
@@ -15,14 +16,31 @@ public class NeuralNet {
     
     private ArrayList<ArrayList<Vertex>> nodes;
     private ArrayList<ArrayList<Edge>> edges;
+    private static Random generator = new Random(3);
     
     public NeuralNet(int[] layers, boolean[] bias){
         this.nodes = new ArrayList<ArrayList<Vertex>>();
         this.edges = new ArrayList<ArrayList<Edge>>();
+        
         for(int i = 0; i < layers.length; i++){
             if(i != layers.length){
                 if(bias[i]){
-                    
+                    nodes.get(i).add(new Vertex(1));
+                }
+                for(int j = 0; j < layers[i]; j++){
+                    nodes.get(i).add(new Vertex());
+                }
+            }
+            else{
+                for(int j = 0; j < layers[i]; j++){
+                    nodes.get(i).add(new Vertex());
+                }
+            }
+        }
+        for(int i = 0; i < nodes.size() - 1; i++){
+            for(int j = 0; j < nodes.get(i).size(); j++){
+                for(int k = 0; k < nodes.get(i+1).size(); k++){
+                    edges.get(i).add(new Edge(nodes.get(i).get(j), nodes.get(i+1).get(k), generator.nextDouble()*2));
                 }
             }
         }
