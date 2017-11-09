178b69637b3b799b3cc5f04c2cba433c7d819549
J. Scheidt <j.h.h.scheidt@gmail.com>
2017-11-09 13:29:40 +0100

    Added names data files

--- src/main/java/lab2/RNN.java
+++ src/main/java/lab2/RNN.java
@@ -1,9 +1,29 @@
 package lab2;
+
+import org.deeplearning4j.nn.conf.layers.BaseRecurrentLayer;
+import org.deeplearning4j.nn.conf.layers.GravesLSTM;
 import org.deeplearning4j.nn.multilayer.MultiLayerNetwork;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.Scanner;
 
 public class RNN {
 
     RNN() {
+        importData();
 
+    }
+
+    private Object[] importData() {
+
+        try{
+            Scanner in = new Scanner(new FileReader("boyNames.txt"));
+        }
+        catch(FileNotFoundException e) {
+            System.out.println("error");
+        }
+
+        return new Object[]{};
     }
 }
